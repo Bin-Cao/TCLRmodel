c3e12e50dc3695e214227590817d252fbb80e43e
Bin Cao <bcao@shu.edu.cn>
2022-05-11 23:14:05 +0800

    Add files via upload

---
+++ Source Code/Execution template/py/template.py
@@ -0,0 +1,34 @@
+if __name__ == "__main__":
+
+    import sys
+    import os
+    print(os.getcwd())
+    # Configure a temporary address
+    # locdir is the address of your computer where the folder 'TreeClassifierforLinearRegression' is located
+    locdir = 'C:\\Users\\Jacob\\Desktop\\TCLRUPLOAD\\Source code'
+    sys.path.append(locdir)
+    import TCLRmodel.TCLRalgorithm as TCLR
+
+
+
+    dataSet = "testdata.csv"
+    # :param dataSet：the input dataset
+    correlation = 'PearsonR(+)'
+    #  :param correlation : {'PearsonR(+)','PearsonR(-)',''MIC','R2'}，default PearsonR(+)
+    #     * PearsonR: (+)(-). for linear relationship
+    #     * MIC for no-linear relationship
+    #     * R2 for no-linear relationship、
+    minsize = 3
+    #  :param minsize : a int number, the minimum number of data in each leave.
+    threshold = 0.9
+    #  :param threshold : a float, less than or equal to 1, default 0.95 for PearsonR.
+    #   To avoid overfitting, threshold = 0.5 is suggested for MIC 0.5.
+    mininc = 0.01
+    #  :param mininc :Minimum expected gain of objective function
+
+
+
+    TCLR.start(dataSet, correlation, minsize, threshold, mininc)
+
+
+
---
+++ Source Code/Execution template/readme.txt
@@ -0,0 +1,8 @@
+Three documents are provided for user:
+<1> .py
+   A template can be executed directly by IDEs: Pycharm, VScode etc.
+<2> .ipynb
+   A template can be executed directly by IDEs: Jupyter notebook, Jupyter lab etc.
+   Be careful that it contains some grammatical parts in markdown.
+<3> .pdf
+   Easy to read but can not be executed directly.
