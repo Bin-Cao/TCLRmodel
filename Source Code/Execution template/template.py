ebf7f62ba348a3661338c97967d5a16bbcdb7ea9
Bin Cao <bcao@shu.edu.cn>
2022-06-12 06:38:13 +0800

    Add files via upload

---
+++ Source Code/Execution template/template.py
@@ -0,0 +1,21 @@
+from TCLR import TCLRalgorithm as model
+"""
+# :param dataSet：the input dataset
+# :param correlation : {'PearsonR(+)','PearsonR(-)',''MIC','R2'}，default PearsonR(+)
+#     * PearsonR: (+)(-). for linear relationship
+#     * MIC for no-linear relationship
+#     * R2 for no-linear relationship
+#  :param minsize : a int number, minimum unique values for linear features of data on each leaf.
+#  :param threshold : a float, less than or equal to 1, default 0.95 for PearsonR.
+#     To avoid overfitting, threshold = 0.5 is suggested for MIC 0.5.
+#  :param mininc :Minimum expected gain of objective function
+"""
+dataSet = "testdata.csv"
+correlation = 'PearsonR(+)'
+minsize = 3
+threshold = 0.9
+mininc = 0.01
+model.start(dataSet, correlation, minsize, threshold, mininc)
+
+
+
