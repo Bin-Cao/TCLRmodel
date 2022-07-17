f9a8585e6f22deb32855250619f8b91ffa451c90
Bin Cao <bcao@shu.edu.cn>
2022-07-17 16:36:28 +0800

    code

---
+++ TCLR/__init__.py
@@ -0,0 +1,21 @@
+print(' Tree classifier for linear regression \n 8 August 2021, version 1, Bin Cao, MGI, SHU, Shanghai, CHINA.')
+
+__version__ = '1.4.4'
+__description__ = 'Tree-Classifier for linear regression'
+__author__ = 'Bin Cao'
+__author_email__ = 'baco@shu.edu.cn'
+__url__ = 'https://github.com/Bin-Cao/TCLRmodel'
+
+
+"""
+The entire feature space is divided into disjointed unit intervals by hyperplanes parallel to the coordinate axes.
+In each partition, TCLR models target y as the function of a feature
+TCLR choses the features and split-point to attain the best fit and recursive binary partitions the space,
+until some stopping rules are applied.
+
+
+Algorithm Patent No. : 2021SR1951267, China
+Reference : Domain knowledge guided interpretive machine learning ——  Formula discovery for the oxidation behavior of Ferritic-Martensitic steels in supercritical water. Bin Cao et al., 2022, JMI, journal paper.
+DOI : 10.20517/jmi.2022.04
+
+"""
